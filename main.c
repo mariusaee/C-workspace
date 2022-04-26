c4636736becf37ae6c2b69bbf331eddded6eb03d
Marius <mariusae@me.com>
2022-04-26 13:08:41 +0300

    My first compilled program

--- main.c
+++ main.c
@@ -16,6 +16,24 @@ void login_user(person_t *user);
 // MARK: - Implementations
 
 int main(int argc, char * argvp[]) {
+    person_t user;
+    login_user(&user);
 
     return 0;
+}
+
+void login_user(person_t *user) {
+    char buffer[256];
+
+    puts("What is your name?");
+    scanf("%s", buffer);
+    printf("Hello, %s!\n", buffer);
+
+    user->name = malloc(sizeof(char) * (strlen(buffer) + 1));
+    user->name = strcpy(user->name , buffer);
+
+    puts("How old are you?");
+    scanf("%lu", &user->age);
+
+    printf("Nice, %lu", user->age);
 }
