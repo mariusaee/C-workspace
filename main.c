1428bbfde22f2ec6fe0ffe488c9cc304e5824950
Marius <mariusae@me.com>
2022-04-26 12:51:10 +0300

    Prototype

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
+#include<stdlib.h>
 #include<stdio.h>
+#include<string.h>
 
-int main() {
-    printf("Hello");
+// MARK: - Defenitions
+
+typedef struct {
+    char *name;
+    size_t age;
+} person_t;
+
+// MARK: - Prototypes
+
+void login_user(person_t *user);
+
+// MARK: - Implementations
+
+int main(int argc, char * argvp[]) {
+
     return 0;
 }
